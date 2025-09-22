8a4b7780b397926458ed7b2006db228995599797
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-09-22 20:47:44 +0530

    migrate DB

---
+++ packages/db/prisma/migrations/20250922145755_set_up/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Element" ADD COLUMN     "bold" BOOLEAN,
+ADD COLUMN     "fontFamily" TEXT,
+ADD COLUMN     "fontSize" DOUBLE PRECISION,
+ADD COLUMN     "italic" BOOLEAN,
+ADD COLUMN     "rotation" DOUBLE PRECISION,
+ADD COLUMN     "text" TEXT,
+ADD COLUMN     "textAlign" TEXT,
+ADD COLUMN     "textColor" TEXT;
