821885af937d576e01a4364037a6bd2fcce2f86b
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2026-01-18 23:53:37 +0530

    fix the url issue

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 export PATH="/home/ubuntu/.nvm/versions/node/v24.12.0/bin:$PATH"
 
-# pnpm install
-# pnpm run build
+cd CollabDraw
 
-# pm2 restart fe-server
-# pm2 restart http-server
-# pm2 restart ws-server
+pnpm install
+pnpm run build
+
+pm2 restart fe-server
+pm2 restart http-server
+pm2 restart ws-server
 
 echo "Deployment completed!, Done check ✅"
