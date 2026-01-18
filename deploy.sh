d63be7a188406ec1471c28f31bd16daf54f7e2e0
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2026-01-18 23:25:49 +0530

    add new CI/CD pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export PATH="/home/ubuntu/.nvm/versions/node/v24.12.0/bin:$PATH"
 
-pnpm install
-pnpm run build
+# pnpm install
+# pnpm run build
 
-pm2 restart fe-server
-pm2 restart http-server
-pm2 restart ws-server
+# pm2 restart fe-server
+# pm2 restart http-server
+# pm2 restart ws-server
 
 echo "Deployment completed!, Done check ✅"
